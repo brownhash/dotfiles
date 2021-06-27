0360cd1975cf1add6dcb7c9857ab7473943549c2
Harshit Sharma <sharma1612harshit@gmail.com>
2021-06-27 19:53:55 +0530

    add more cli setups

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
+username=$(whoami)
+
 # Space vim
 echo 'Setting up SpaceVim'
 curl -sLf https://spacevim.org/install.sh | bash
 echo 'For more visit - https://spacevim.org/quick-start-guide/#linux-and-macos'
+
+# Zsh
+echo 'Setting up Zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'For more visit - https://github.com/ohmyzsh/ohmyzsh'
+
+# Auto suggestions
+echo 'Setting up Zsh Autosuggestions'
+git clone https://github.com/zsh-users/zsh-autosuggestions /Users/${username}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+echo 'For more visit - https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md'
